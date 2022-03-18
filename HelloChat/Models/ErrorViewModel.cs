37cf85212d54c4e438528c1de1d610e571a757de
Ninad Ambade <ninad.ambade@aploris.com>
2022-03-18 14:10:11 +0530

    Add project files.

---
+++ HelloChat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
